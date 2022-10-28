81337f6538c1071f2a1397ddc919b3c24351f184
lyrickhare <80460792+lyrickhare@users.noreply.github.com>
2022-10-28 14:59:55 +0530

    idk what is this but just commitimg

---
+++ sem5 (copy)/AE314/tensile test/ram.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct ram
+{
+    double a;
+    double b;
+};
+
+int main(){
+ram *p=new ram;
+p->a=10;
+p->b=20;
+cout<<p->a<<"\t"<<p->b<<endl;
+
+}
---
+++ sem5 (copy)/AE314/tensile test/ram.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'checkMagazine' function below.
+#
+# The function accepts following parameters:
+#  1. STRING_ARRAY magazine
+#  2. STRING_ARRAY note
+#
+
+def checkMagazine(magazine, note):
+    # Write your code here
+    for word in note:
+        if(word in magazine and magazine.count(word)>note.count(word)):
+            print(word)
+        else:
+            return "No"
+    return "Yes"
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    m = int(first_multiple_input[0])
+
+    n = int(first_multiple_input[1])
+
+    magazine = input().rstrip().split()
+
+    note = input().rstrip().split()
+
+    print(checkMagazine(magazine, note)+"\n")
